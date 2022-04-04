771764b6a3b85b0f18e21e406a3f872b892cc3e3
INoorik <ajnurabuzarov8@gmail.com>
2022-04-04 22:29:44 +0300

    size change on click added

--- js/script.js
+++ js/script.js
@@ -10,8 +10,13 @@ function validateInput(object) {
   });
 }
 
+let is_increased = false;
 $(document).ready(function() {
     validateInput($('#min_size'));
     validateInput($('#max_size'));
     changeSize($('#box'), $('#min_size').val(), 0);
+    $('#box').click(function() {
+      changeSize($('#box'), $(is_increased?'#min_size':'#max_size').val(), 1000);
+      is_increased = !is_increased;
+    });
 });
