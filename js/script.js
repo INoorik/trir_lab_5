85c67deba9d0311e22a2ee060d9496d983c576bf
INoorik <ajnurabuzarov8@gmail.com>
2022-04-05 01:08:37 +0300

    box random movement added

--- js/script.js
+++ js/script.js
@@ -10,13 +10,25 @@ function validateInput(object) {
   });
 }
 
+function generateRandomCoordinates() {
+  let coordinates = {
+    x: Math.round(Math.random() * $(document).width()),
+    y: Math.round(Math.random() * $(document).height())
+  };
+  return coordinates;
+}
+
 let is_increased = false;
 $(document).ready(function() {
     validateInput($('#min_size'));
     validateInput($('#max_size'));
     changeSize($('#box'), $('#min_size').val(), 0);
     $('#box').click(function() {
-      changeSize($('#box'), $(is_increased?'#min_size':'#max_size').val(), 1000);
+      changeSize($('#box'), $(is_increased?'#min_size':'#max_size').val(), 5000);
       is_increased = !is_increased;
     });
+    setInterval(function() {
+      let coordinates = generateRandomCoordinates();
+      $('#box').animate({left: coordinates.x, top: coordinates.y}, 5000);
+    }, 1000);
 });
