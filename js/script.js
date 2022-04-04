2f48a1ef3f61664f735e9784fcd12193adbc38ce
INoorik <ajnurabuzarov8@gmail.com>
2022-04-04 17:46:28 +0300

    User input validating added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 function changeSize(object, size, time) {
   object.animate({ width:size, height:size}, time);
 }
+
+function validateInput(object) {
+  let rightVariant = ''
+  for(let symbol of object[0].value) {
+    if(/\d/.test(symbol)) {
+      rightVariant += symbol;
+    }
+  }
+  object[0].value = rightVariant;
+}
+
+$(document).ready(function() {
+  $('#min_size').keyup(function(){
+    validateInput($('#min_size'));
+  });
+});
