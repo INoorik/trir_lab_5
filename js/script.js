a361add68ff4a3d611a4a1a72c5a00d1cdcce6e7
INoorik <ajnurabuzarov8@gmail.com>
2022-04-05 15:02:01 +0300

    Added a check that the entered minimum is less than the maximum

--- js/script.js
+++ js/script.js
@@ -19,16 +19,34 @@ function generateRandomCoordinates() {
   return coordinates;
 }
 
+function isUserInputCorrect(minSizeObject, maxSizeObject) {
+  let numberRegExp = /^ *\d+ *$/;
+  if(!numberRegExp.test(minSizeObject.val()) ||
+   !numberRegExp.test(maxSizeObject.val())){
+     return false;
+   }
+  if(minSizeObject.val()-0 > maxSizeObject.val()-0) {
+    return false;
+  }
+  return true;
+}
+
 let is_increased = false;
 $(document).ready(function() {
     preventNonNumbers($('#min_size'));
     preventNonNumbers($('#max_size'));
     $('#start_button').click(function(){
+      if(!isUserInputCorrect($('#min_size'), $('#max_size'))) {
+        alert('Введите, пожалуйста, меньший размер в поле для минимального \
+          размера, а большее - в поле для максимально');
+        return;
+      }
       $('form').hide();
       $('#box').show();
       changeSize($('#box'), $('#min_size').val(), 0);
       $('#box').click(function() {
-        changeSize($('#box'), $(is_increased?'#min_size':'#max_size').val(), 1000);
+        changeSize($('#box'), $(is_increased?'#min_size':'#max_size').val(),
+          1000);
         is_increased = !is_increased;
       });
       setInterval(function() {
