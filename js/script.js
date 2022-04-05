8cc7959f36962719d10d692e7ec3bfad22ce8c25
INoorik <ajnurabuzarov8@gmail.com>
2022-04-05 14:47:09 +0300

    Start button added

--- js/script.js
+++ js/script.js
@@ -23,17 +23,22 @@ let is_increased = false;
 $(document).ready(function() {
     validateInput($('#min_size'));
     validateInput($('#max_size'));
-    changeSize($('#box'), $('#min_size').val(), 0);
-    $('#box').click(function() {
-      changeSize($('#box'), $(is_increased?'#min_size':'#max_size').val(), 1000);
-      is_increased = !is_increased;
+    $('#start_button').click(function(){
+      $('form').hide();
+      $('#box').show();
+      changeSize($('#box'), $('#min_size').val(), 0);
+      $('#box').click(function() {
+        changeSize($('#box'), $(is_increased?'#min_size':'#max_size').val(), 1000);
+        is_increased = !is_increased;
+      });
+      setInterval(function() {
+        let coordinates = generateRandomCoordinates();
+        $('#box').animate({left: coordinates.x, top: coordinates.y}, 2000);
+      }, 3000);
+      setInterval(function() {
+        $('#coordinates').text(`${Math.round($('#box').position().left)}:${
+          Math.round($('#box').position().top)}`);
+      }, 100);
     });
-    setInterval(function() {
-      let coordinates = generateRandomCoordinates();
-      $('#box').animate({left: coordinates.x, top: coordinates.y}, 2000);
-    }, 3000);
-    setInterval(function() {
-      $('#coordinates').text(`${Math.round($('#box').position().left)}:${
-        Math.round($('#box').position().top)}`);
-    }, 100);
+
 });
